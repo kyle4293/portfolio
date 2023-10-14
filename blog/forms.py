dd38185476a8daf26575ad4846d474308065e7f5
최창규 <chang4293@gmail.com>
2023-10-14 15:43:46 +0900

    postform

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from blog.models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post  # 사용할 모델
+        fields = ['subject', 'content']  # PostForm에서 사용할 Post 모델의 속성
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def index(request):
     post_list = Post.objects.order_by('-create_date')
@@ -17,3 +18,8 @@ def comment_create(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     post.comment_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('blog:detail', post_id=post_id)
+
+def post_create(request):
+    form = PostForm()
+    context = {'form':form}
+    return render(request, 'blog/post_form.html', context)
