14fc072332d8a08ede6da0717abf6277df06c986
최창규 <chang4293@gmail.com>
2023-10-14 15:09:03 +0900

    1

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+
+def index(request):
+    post_list = Post.objects.order_by('-create_date')
+    context = {'posts': post_list}
+    return render(request, 'blog/post_list.html', context)
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post':post}
+    return render(request, 'blog/post_detail.html', context)
+
+def comment_create(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.comment_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('blog:detail', post_id=post_id)
